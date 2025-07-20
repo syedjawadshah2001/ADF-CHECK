07fb4b90ec3e6c7706984a624cc7a645054865b4
Syed Jawad Shah <114677255+syedjawadshah2001@users.noreply.github.com>
2025-07-20 18:56:35 +0500

    Add files via upload

--- utilities/db.py
+++ utilities/db.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore, auth
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("./utilities/adf-check-firebase-adminsdk-s0rox-579c070b81.json")
+    cred = credentials.Certificate("./utilities/adf-check-firebase-adminsdk-s0rox-f5fff88f46.json")
     firebase_admin.initialize_app(cred)
 
 

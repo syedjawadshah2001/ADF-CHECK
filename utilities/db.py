c32dafcd0bc59a9fc278c83b068493262bfbdd32
Syed Jawad Shah <114677255+syedjawadshah2001@users.noreply.github.com>
2025-07-20 22:05:31 +0500

    updated

--- utilities/db.py
+++ utilities/db.py
@@ -1,7 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials, firestore, auth
 
-if not firebase_admin._apps:
+try:
+    # Try to get the already initialized app
+    firebase_admin.get_app()
+except ValueError:
+    # If it doesn't exist, initialize it
     cred = credentials.Certificate("./utilities/adf-check-firebase-adminsdk-s0rox-f5fff88f46.json")
     firebase_admin.initialize_app(cred)
 

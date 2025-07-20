17a2df1ad84c21fa83d45fd28764d118b82f348e
Syed Jawad Shah <114677255+syedjawadshah2001@users.noreply.github.com>
2025-07-20 22:07:57 +0500

    modified

--- utilities/db.py
+++ utilities/db.py
@@ -1,13 +1,14 @@
+import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore, auth
 
-try:
-    # Try to get the already initialized app
-    firebase_admin.get_app()
-except ValueError:
-    # If it doesn't exist, initialize it
+@st.cache_resource
+def init_firebase():
     cred = credentials.Certificate("./utilities/adf-check-firebase-adminsdk-s0rox-f5fff88f46.json")
-    firebase_admin.initialize_app(cred)
+    return firebase_admin.initialize_app(cred)
 
+# Initialize Firebase safely
+init_firebase()
 
+# Now use Firestore
 db = firestore.client()
